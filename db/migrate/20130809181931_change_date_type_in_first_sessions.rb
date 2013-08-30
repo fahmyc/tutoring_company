8e21905d1b5089eeebf2d2ee7f97b805659d344a
Chris Fahmy <chrisfahmy@hotmail.com>
2013-08-30 00:18:59 -0400

    fixed db

--- db/migrate/20130809181931_change_date_type_in_first_sessions.rb
+++ db/migrate/20130809181931_change_date_type_in_first_sessions.rb
@@ -1,7 +1,14 @@
 class ChangeDateTypeInFirstSessions < ActiveRecord::Migration
   def self.up
-  	change_column :first_sessions, :date, :date
-  end
+  connection.execute(%q{
+    alter table first_sessions
+    alter column date
+    type date using cast(date as date)
+  })
+end
+
+  #	change_column :first_sessions, :date, :date
+  #end
 
   def self.down
   	change_column :first_sessions, :date, :string
